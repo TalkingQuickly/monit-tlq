cca8d0c37eb4077aa5ad013f05d4eb462ecb11cf
Ben Dixon <ben@hillsbede.co.uk>
2013-10-20 16:54:03 +0100

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,2 @@
+name "monit-tlq"
+supports "ubuntu"
---
+++ recipes/default.rb
@@ -0,0 +1,73 @@
+# install monit
+package 'monit'
+
+# update the main monit configuration file
+template "/etc/monit/monitrc" do
+  owner "root"
+  group "root"
+  mode "0700"
+  source "monit-rc.erb"
+end
+
+# disable traditional init.d way of managing monit
+bash "disabling init.d script for monit" do
+  user "root"
+  code <<-EOC
+/etc/init.d/monit stop
+update-rc.d -f monit remove
+  EOC
+end
+
+# Use upstart to manage monit
+
+file '/etc/init/monit.conf' do
+  owner "root"
+  group "root"
+  mode "0644"
+  content <<-EOF
+
+# after adding this file run
+#   initctl reload-configuration
+#
+# You can manually start and stop monit like this:
+#
+# start monit
+# stop monit
+#
+
+description "Monit service manager"
+
+limit core unlimited unlimited
+
+start on runlevel [2345]
+stop on runlevel [!2345]
+
+expect daemon
+respawn
+
+exec /usr/bin/monit -c /etc/monit/monitrc
+
+pre-stop exec /usr/bin/monit -c /etc/monit/monitrc quit
+  EOF
+end
+
+# allow monit to startup
+template '/etc/default/monit' do
+  owner "root"
+  group "root"
+  mode "0644"
+  source "allow-monit-start.erb"
+end
+
+execute "initctl reload-configuration"
+
+bash "opening ufw for monit traffic" do
+  user "root"
+  code <<-EOC
+ufw allow 2812
+ufw --force enable
+ufw allow 2812
+  EOC
+end
+
+execute "service monit restart"
