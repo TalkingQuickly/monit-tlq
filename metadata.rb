3bcab55e20d2df8a392d8c33c3f469b9d0bdad36
Ben Dixon <ben@hillsbede.co.uk>
2013-11-03 18:44:22 +0000

    fixes indentation of main config file and mailserver definition format

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name              "monit-tlq"
 maintainer        "Ben Dixon"
 maintainer_email  "ben@talkingquickly.co.uk"
 description       "Basic monit installation"
-version           "0.3.1"
+version           "0.3.2"
 
 supports      "ubuntu"
