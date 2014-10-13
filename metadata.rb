95d5c36fdb670b034550c181b51f169b92d2935f
Ben Dixon <ben@talkingquickly.co.uk>
2014-10-13 13:46:41 +0100

    add ppid to minimise alerts

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name              "monit-tlq"
 maintainer        "Ben Dixon"
 maintainer_email  "ben@talkingquickly.co.uk"
 description       "Basic monit installation"
-version           "0.3.8"
+version           "0.3.9"
 
 supports      "ubuntu"
