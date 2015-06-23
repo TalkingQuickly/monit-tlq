28bbdc06702a00c5cb5ace1fd7f79a51a127ed0c
Ben Dixon <ben@hillsbede.co.uk>
2015-06-23 16:59:47 +0100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name              "monit-tlq"
 maintainer        "Ben Dixon"
 maintainer_email  "ben@talkingquickly.co.uk"
 description       "Basic monit installation"
-version           "0.3.10"
+version           "0.3.11"
 
 supports      "ubuntu"
