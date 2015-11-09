3c2ebb02fa7a84e8e2c95a72b6527f4720d2c886
James Nachbar <nachbar@plastic.org>
2015-11-09 07:47:30 -0700

    fix unmatched quote, and allow for from address, etc. config

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,5 @@
 default[:monit][:notify_emails] = false
+default[:monit][:mailserver][:emailfrom] = "emailfrom@example.com"
+default[:monit][:mailserver][:emailreplyto] = "emailreplyto@example.com"
+default[:monit][:mailserver][:emailsubject] = "$SERVICE $EVENT at $DATE"
+default[:monit][:mailserver][:emailmessage] = "Monit $ACTION $SERVICE at $DATE on $HOST: $DESCRIPTION.\nYours sincerely,\nmonit"
