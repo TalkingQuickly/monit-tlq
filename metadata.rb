ef62415f130373ebfb7e407023b8de8d29581b6b
Ben <ben@talkingquickly.co.uk>
2021-03-01 04:24:33 +0000

    updates: Ubuntu 20.04 LTS compat

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,5 @@ name              "monit-tlq"
 maintainer        "Ben Dixon"
 maintainer_email  "ben@talkingquickly.co.uk"
 description       "Basic monit installation"
-version           "0.3.11"
-
+version           "0.4.0"
 supports      "ubuntu"
--- recipes/default.rb
+++ recipes/default.rb
@@ -10,35 +10,8 @@ template "/etc/monit/monitrc" do
   notifies :run, "execute[restart-monit]", :immediately
 end
 
-# disable traditional init.d way of starting monit
-bash "disabling init.d script for monit" do
-  user "root"
-  code <<-EOC
-    update-rc.d -f monit remove
-  EOC
-  #  /etc/init.d/monit stop
-end
-
-# Use upstart to manage monit
-template '/etc/init/monit.conf' do
-  owner "root"
-  group "root"
-  mode "0644"
-  source "monit-upstart.conf.erb"
-  notifies :run, "execute[restart-monit]", :immediately
-end
-
-# allow monit to startup
-template '/etc/default/monit' do
-  owner "root"
-  group "root"
-  mode "0644"
-  source "allow-monit-start.erb"
-  notifies :run, "execute[restart-monit]", :immediately
-end
-
 execute "restart-monit" do
-  command "initctl reload-configuration"
+  command "systemctl restart monit"
   command "monit reload"
   action :nothing
 end
